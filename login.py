9d556477b4f864b4da03e2502d03a55821af9c13
Sumeet <sumeetshrivas@yahoo.com>
2020-12-04 15:11:05 +0530

    Optimize code

--- login.py
+++ login.py
@@ -1,16 +1,6 @@
 import ast
-import mysql.connector
 
-mydb = mysql.connector.connect(
-  host = 'localhost',
-  user = 'root',
-  passwd = 'sumeet@1996',
-  database = 'python_login'
-)
-
-mycursor = mydb.cursor()
-
-def login(email,passw):
+def login(email,passw, mycursor):
   sqlFormulaSELECT = 'SELECT * FROM user WHERE email = %s'
   mycursor.execute(sqlFormulaSELECT,(email,))
   myresult = mycursor.fetchall()
@@ -20,9 +10,9 @@ def login(email,passw):
       print("Name is {} with email {} and password {}".format(result[0], result[1], result[2]))
     else:
       print("*Wrong Credentials, please try again!!!*")
-      main_login()
+      main_login(mycursor)
 
-def main_login():
+def main_login(cursor):
   email = input("Enter Email: ")
   passw = input("Enter Password: ")
-  login(email, passw)
+  login(email, passw, cursor)
--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from login import main_login
 from signup import main_signup
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host = 'localhost',
+  user = 'root',
+  passwd = 'sumeet@1996',
+  database = 'python_login'
+)
+
+mycursor = mydb.cursor()
 
 
 def get_selection(selection):
   checker = False
   while checker == False:
     if selection.lower() == 'login' or selection.lower() == 'l':
-      main_login()
+      main_login(mycursor)
       checker = True
     elif selection.lower() == 'signup' or selection.lower() == 's':
       main_signup()
       selection_2 = input("Do you want to sign in now? (Y/N): ")
       if selection_2.lower() == 'y':
-        main_login()
+        main_login(mycursor)
       elif selection_2.lower() == 'n':
         break
       checker = True
